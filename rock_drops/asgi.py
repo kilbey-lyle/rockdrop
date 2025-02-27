165d3f7d3ae96f42008965c2bed39dd0455d1ec5
LyleKilbey <133753600+LyleKilbey@users.noreply.github.com>
2025-02-27 10:19:18 +0000

    feat: added main app and super user

---
+++ rock_drops/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rock_drops project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rock_drops.settings')
+
+application = get_asgi_application()
