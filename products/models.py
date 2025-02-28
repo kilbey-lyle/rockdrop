952a07f373a6d6e5474de82a86f03514e99cfc88
LyleKilbey <133753600+LyleKilbey@users.noreply.github.com>
2025-02-28 17:39:49 +0000

    feat: added sorting

--- products/models.py
+++ products/models.py
@@ -6,16 +6,25 @@ CAT_CHOICES = {
     "NL": "necklace",
 }
 
+STONE_CHOICES = {
+    "RG": "ring",
+    "CF": "cuff",
+    "NL": "necklace",
+}
+
+
+
  
 class Product(models.Model):
     category = models.CharField(max_length=50, null=True, choices=CAT_CHOICES)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     is_public = models.BooleanField(default=False)
+    stone_type = 
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product
 
 # Create your views here.
@@ -6,10 +10,58 @@ from .models import Product
 def all_products(request):
     '''A view to show all products'''
 
-    products = Product.objects.filter(is_public=True)
+    products = Product.objects.all()
+    query = None
+    category = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category=category)
+
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search results")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
-        'products': products
+        'products': products,
+        'current_category': category,
+        'current_sorting':current_sorting,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
+
+def product_details(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
