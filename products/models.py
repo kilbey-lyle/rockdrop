8e93a84400f74551f2d42e37c42ccd79215ba44f
LyleKilbey <133753600+LyleKilbey@users.noreply.github.com>
2025-02-28 17:54:34 +0000

    feat:added verbose feild to product model

--- products/models.py
+++ products/models.py
@@ -1,18 +1,46 @@
 from django.db import models
 
 CAT_CHOICES = {
-    "RG": "ring",
-    "CF": "cuff",
-    "NL": "necklace",
+    "RG": "Ring",
+    "CF": "Cuff",
+    "NL": "Necklace",
 }
 
 STONE_CHOICES = {
-    "RG": "ring",
-    "CF": "cuff",
-    "NL": "necklace",
+    "DI": "Diamond",
+    "PR": "Pearl",
+    "QZ": "Quartz",
+    "TP": "Topaz",
+    "SYN": "Synthetic"
 }
 
+METAL_CHOICES = {
+    "GL": "Gold",
+    "RG": "Rose Gold",
+    "YG": "Yellow Gold",
+    "WG": "White Gold",
+    "SS": "Sterling Silver",
+    "BRZ": "Bronze",
+    "TIT": "Titanium"
+}
 
+CHAIN_LENGTH_CHOICES = {
+    "16": "16 Inches",
+    "18": "18 Inches",
+    "20": "20 Inches",
+    "22": "22 Inches",
+    "24": "24 Inches",
+    "26": "26 Inches",
+    "28": "28 Inches",    
+}
+
+CHAIN_THICKNESS_CHOICES = {
+    "1": "1mm",
+    "2": "2mm",
+    "3": "3mm",
+    "4": "4mm",
+    "5": "5mm",    
+}
 
  
 class Product(models.Model):
@@ -23,8 +51,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    stone_type = models.CharField(max_length=50, null=True, choices=STONE_CHOICES)
+    metal_type = models.CharField(max_length=50, null=True, choices=METAL_CHOICES)
+    chain_length = models.CharField(max_length=50, null=True, choices=CHAIN_LENGTH_CHOICES)
+    chain_thickness = models.CharField(max_length=50, null=True, choices=CHAIN_THICKNESS_CHOICES)
+    has_sizes = models.BooleanField(default=False)
     is_public = models.BooleanField(default=False)
-    stone_type = 
 
     def __str__(self):
         return self.name
